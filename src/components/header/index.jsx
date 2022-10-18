0337f672b9294b0fc3ad7a928d1cddee86ee7a4b
Fernandes <jose.fernandes@hp.com>
2022-10-18 01:57:15 -0300

    create header + styles

---
+++ src/components/header/index.jsx
@@ -0,0 +1,11 @@
+import { Header, Logo } from "./styles";
+
+const AppHeader = () => {
+  return (
+    <Header>
+      <Logo>Expenses</Logo>
+    </Header>
+  );
+};
+
+export default AppHeader;
---
+++ src/components/header/styles.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin: 0px 25px;
+`;
+
+export const Logo = styled.p`
+  font-family: "Arial";
+  font-style: normal;
+  font-weight: 800;
+  font-size: 36px;
+  line-height: 58px;
+  color: #272639;
+`;
