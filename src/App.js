1d9a023c1f86add30dbb0c3880fd37b38464868b
Fernandes <jose.fernandes@hp.com>
2022-10-25 00:53:38 -0300

    clean app.js code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import GlobalStyle from "./global/globalStyles";
 import AppHeader from "./components/header";
@@ -6,35 +5,12 @@ import Expenses from "./components/expenses/index";
 import ExpensesForm from "./components/expensesForm";
 
 function App() {
-  const [expensesArray, setExpensesArray] = useState([
-    {
-      id: 1,
-      title: "Macbook Air M1",
-      price: 1535.49,
-      date: new Date(2022, 1, 15),
-    },
-    {
-      id: 2,
-      title: "Galaxy S22 Ultra",
-      price: 1537.98,
-      date: new Date(2021, 11, 15),
-    },
-    {
-      id: 3,
-      title: "Smart TV",
-      price: 3589.73,
-      date: new Date(2020, 7, 22),
-    },
-  ]);
   return (
     <>
       <GlobalStyle />
       <AppHeader />
-      <ExpensesForm
-        expensesArray={expensesArray}
-        expensesArraySetter={setExpensesArray}
-      />
-      <Expenses expensesArray={expensesArray} />
+      <ExpensesForm />
+      <Expenses />
     </>
   );
 }
